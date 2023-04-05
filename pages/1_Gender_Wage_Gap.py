c94937d163d7f9854a3d6636a5f355312255b7ed
Miranda Cooper <mleighc@umich.edu>
2023-04-05 14:57:36 -0400

    set up tab structure

---
+++ pages/1_Gender_Wage_Gap.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import altair as alt
+import bokeh as b
+import pandas as pd
+
+st.set_page_config(page_title="Gender Wage Gap", page_icon="⚖️")
+
+st.markdown("# Education and the gender wage gap")
+st.sidebar.header("Gender Wage Gap")
+st.write(
+    """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+    culpa qui officia deserunt mollit anim id est laborum."""
+)
---
+++ pages/2_Earning_Potential.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import altair as alt
+import bokeh as b
+import pandas as pd
+
+st.set_page_config(page_title="Earning Potential", page_icon="💰")
+
+st.markdown("# Higher Education Degrees and Earning Potential")
+st.sidebar.header("Earning Potential")
+st.write(
+    """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+    culpa qui officia deserunt mollit anim id est laborum."""
+)
