3716ed44fe12bd1afa295d8afdb64a2ef3f7c238
Miranda Cooper <mleighc@umich.edu>
2023-04-05 14:17:02 -0400

    Change streamlit_app to Home

---
+++ Home.py
@@ -0,0 +1,39 @@
+# import os
+
+# import altair as alt
+# import numpy as np
+# import pandas as pd
+# import streamlit as st
+
+# st.title('FILLER TITLE')
+# st.write('By Mike and Miranda')
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="Welcome",
+    page_icon="📚",
+)
+
+st.write("# Welcome to the Main Page! 👋")
+
+# st.sidebar.success("Select a demo above.")
+st.markdown('Lorem...')
+
+st.markdown(
+    """
+    Streamlit is an open-source app framework built specifically for
+    Machine Learning and Data Science projects.
+    **👈 Select a demo from the sidebar** to see some examples
+    of what Streamlit can do!
+    ### Want to learn more?
+    - Check out [streamlit.io](https://streamlit.io)
+    - Jump into our [documentation](https://docs.streamlit.io)
+    - Ask a question in our [community
+        forums](https://discuss.streamlit.io)
+    ### See more complex demos
+    - Use a neural net to [analyze the Udacity Self-driving Car Image
+        Dataset](https://github.com/streamlit/demo-self-driving)
+    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+"""
+)
