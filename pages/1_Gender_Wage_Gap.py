3c4b1be1557ea8c1d01f3baa466df95379884e05
mcverhulst <verhulst@umich.edu>
2023-04-14 18:02:05 -0400

    - remove pyramid plot - add male base pay to ratio chart - clean old code

--- pages/1_Gender_Wage_Gap.py
+++ pages/1_Gender_Wage_Gap.py
@@ -1,10 +1,6 @@
 import streamlit as st
 import altair as alt
 
-import pandas as pd
-import os
-from utils.util_funcs import load_data
-
 st.set_page_config(page_title="Gender Wage Gap",
                    page_icon="⚖️",
                    layout="wide",
@@ -25,104 +21,62 @@ st.write(
     culpa qui officia deserunt mollit anim id est laborum."""
 )
 
-# FILTER TO SUBSET OF DATA
-data_filtered = data_only[(data_only["group"] == "total men") | (data_only["group"] == "total women")]
-
-# TEST BAR PLOT
-# inspiration: https://altair-viz.github.io/gallery/us_population_pyramid_over_time.html
-# c = (
-#     alt.Chart(data_filtered)
-#     .mark_bar()
-#     .encode(x="year:N", y=alt.Y("value:Q"), color="group:N", tooltip="value")
-# )
-
-# st.write("## Not a good chart, just a test chart")
-# st.altair_chart(c, use_container_width=True)
-
-
-####################
-### PYRAMID PLOT ###
-####################
-
-color_scale = alt.Scale(domain=["total men", "total women"], range=["#1f77b4", "#e377c2"])
-base = alt.Chart(ratio_df).mark_bar().encode(tooltip=["ratio"]).properties(width=250)
-
-# LEFT CHART
-left = base.encode(
-    alt.X("total men:Q", title="wages", sort="descending", scale=alt.Scale(domain=[0, 1200])),
-    alt.Y("year:N", axis=None, sort="descending"),
-    color=alt.value("#31b0a5"),
-)
-
-# RIGHT CHART
-right = base.encode(
-    alt.X("total women:Q", title="wages", sort="ascending", scale=alt.Scale(domain=[0, 1200])),
-    alt.Y("year:N", axis=None, sort="descending"),
-    color=alt.value("#de8b33")
-    # alt.Color('group:O', scale=color_scale)
-)
-
-# MIDDLE CHART
-middle = (
-    base.encode(
-        alt.Y("year:N", axis=None, sort="descending"),
-        alt.Text("year"),
-    )
-    .mark_text()
-    .properties(width=35)
-)
-
-# CONCATING FINAL TREE DIAGRAM
-ch = alt.hconcat(left, middle, right, spacing=5)
-
-# PAY GAP RATIO CHART
-line = (
-    alt.Chart(ratio_df)
-    .mark_line()
-    .encode(
-        x=alt.X("year:N", title="Year", axis=alt.Axis(labelAngle=-45)),
-        y=alt.Y("ratio:Q", scale=alt.Scale(domain=(0.65, 1)), title="Female to male wage ratio"),
-    )
-)
-
-# CONCATING PYRAMID AND RATIO CHARTS
-combo = alt.vconcat(ch, line)
-st.altair_chart(combo, theme="streamlit", use_container_width=True)
-
 #########################
-### Stacked bar chart ###
+### STACKED BAR CHART ###
 #########################
 
 st.write("## Testing2...")
-pink_blue = alt.Scale(domain=("Male", "Female"), range=["#31b0a5", "#de8b33"])  # ['#31b0a5', '#de8b33']
+colors = alt.Scale(domain=("Male", "Female"), range=["#31b0a5", "#de8b33"])  # ['#31b0a5', '#de8b33']
 
 values = st.slider("Select a range of years", 1995, 2020, (1995, 2020))
 
 bar2 = (
-    alt.Chart(data_filtered)
-    .mark_bar()
-    .encode(
+    alt.Chart(data_filtered).mark_bar().encode(
         x=alt.X("group:N", title=None, axis=None),
         y=alt.Y("value:Q", title="Median Weekly Pay", scale=alt.Scale(domain=(0, 1200))),
-        color=alt.Color("group:N", scale=pink_blue),
+        color=alt.Color("group:N", scale=colors),
         column=alt.Column(
             "year:N",
             title=None,
-            header=alt.Header(labelAngle=-45, labelFontSize=13, labelOrient="bottom", labelPadding=35),
-        ),
+            header=alt.Header(labelAngle=-45, labelFontSize=13, labelOrient="bottom", labelPadding=35)),
         tooltip=["group", "value", "year"],
+    ).transform_filter(
+        values[0] <= alt.datum.year
+    ).transform_filter(
+        alt.datum.year <= values[1]
+    ).transform_calculate(
+        "group", alt.expr.if_(alt.datum.group == "total men", "Male", "Female")
     )
-    .properties(width=25)
-    .transform_filter(values[0] <= alt.datum.year)
-    .transform_filter(alt.datum.year <= values[1])
-    .transform_calculate("group", alt.expr.if_(alt.datum.group == "total men", "Male", "Female"))
-    .configure_facet(spacing=8)
 )
 
-st.altair_chart(bar2, theme="streamlit", use_container_width=False)
+# RATIO LINE CHART
+line = alt.Chart(ratio_df).mark_line(point=True).encode(
+        x=alt.X("year:N", title="Year", axis=alt.Axis(labelAngle=-45)),
+        y=alt.Y("ratio:Q", scale=alt.Scale(domain=(0, 1.2)), title="Female to male wage ratio"),
+        color = alt.value("#de8b33")
+).transform_filter(
+    values[0] <= alt.datum.year
+).transform_filter(
+    alt.datum.year <= values[1]
+)
+
+# MALE BASELINE
+equal_pay = alt.Chart(pd.DataFrame({'y': [1]})).mark_rule(
+    strokeWidth=3,
+    strokeDash=[7,2]
+).encode(
+    y='y',
+    color = alt.value("#31b0a5")
+)
+
+line2 = line + equal_pay
+
+combo2 = alt.vconcat(bar2, line2).configure_facet(spacing=8)
+
+st.altair_chart(combo2, theme="streamlit", use_container_width=True)
 
 #####################
-### Women by race ###
+### WOMEN BY RACE ###
 #####################
 
 cuts = ["Total women:Q", "White women:Q", "Hispanic or Lation women:Q", "Black women", "Asian women"]
@@ -132,8 +86,8 @@ title = alt.TitleParams("Median Weekly Earnings in 2022 Dollars", anchor='middle
 base = alt.Chart(male, title=title, height=600).mark_line(point=True, strokeDash=[6,1]).encode(
     x = alt.X("year:N", title="Year", axis=alt.Axis(labelAngle=-45)),
     y = alt.Y("men:Q", scale=alt.Scale(domain=(500, 1400)), title="Median Weekly Wages ($2022)"),
-    color = alt.value("black"),
-)#.interactive()
+    color = alt.value("#31b0a5"),
+)
 
 base_women = alt.Chart(female).mark_line(point=True).encode(
     x = alt.X("year:N", axis=alt.Axis(labelAngle=-45)),
--- preprocessing/gender_ratios.py
+++ preprocessing/gender_ratios.py
@@ -15,6 +15,8 @@ data = pd.melt(data, id_vars=["group"])
 data = data.rename(columns={"variable": "year"})
 data_only = data[:2340]
 
+data_filtered = data_only[(data_only["group"] == "total men") | (data_only["group"] == "total women")]
+
 ratio_df = ratio_df[["group", "1995", "1996", "1997", "1998", "1999", "2000",
                      "2001", "2002", "2003", "2004", "2005", "2006", "2007",
                      "2008", "2009", "2010", "2011", "2012", "2013", "2014",
