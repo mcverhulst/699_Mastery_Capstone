fc6c1d8a4f19bceaacf13da6e579a6b4ac8f4207
mcverhulst <verhulst@umich.edu>
2023-04-14 14:47:13 -0400

    - calculated hours needed to match male pay - cleaned code

--- preprocessing/gender_ratios.py
+++ preprocessing/gender_ratios.py
@@ -1,9 +1,9 @@
 from utils.util_funcs import load_data
 import pandas as pd
 
-#############################
-### male female pay ratio ###
-#############################
+################################
+### FEMALE TO MALE PAY RATIO ###
+################################
 
 data = load_data("edu_wages_2022.csv")
 data.set_index("report_name")
@@ -15,46 +15,28 @@ data = pd.melt(data, id_vars=["group"])
 data = data.rename(columns={"variable": "year"})
 data_only = data[:2340]
 
-ratio_df = ratio_df[
-    [
-        "group",
-        "1995",
-        "1996",
-        "1997",
-        "1998",
-        "1999",
-        "2000",
-        "2001",
-        "2002",
-        "2003",
-        "2004",
-        "2005",
-        "2006",
-        "2007",
-        "2008",
-        "2009",
-        "2010",
-        "2011",
-        "2012",
-        "2013",
-        "2014",
-        "2015",
-        "2016",
-        "2017",
-        "2018",
-        "2019",
-        "2020",
-    ]
-]
+ratio_df = ratio_df[["group", "1995", "1996", "1997", "1998", "1999", "2000",
+                     "2001", "2002", "2003", "2004", "2005", "2006", "2007",
+                     "2008", "2009", "2010", "2011", "2012", "2013", "2014",
+                     "2015", "2016", "2017", "2018", "2019", "2020",]]
+
 ratio_df = ratio_df.T
 ratio_df = ratio_df.tail(-1)
 ratio_df.rename(columns={1: "total men", 2: "total women"}, inplace=True)
+
+# RATIO OF FEMALE PAY TO MALE PAY
 ratio_df["ratio"] = ratio_df["total women"] / ratio_df["total men"]
+
+# HOURS WOMEN NEED TO WORK TO MATCH MEN PAY
+# https://www.geeksforgeeks.org/applying-lambda-functions-to-pandas-dataframe/#
+ratio_df = ratio_df.assign(hours=lambda x: (x["total men"] - x["total women"]) / (x["total women"] / 40))
+
 ratio_df = ratio_df.reset_index()
 ratio_df.rename(columns={"index": "year"}, inplace=True)
 
+
 ##########################
-### Female pay by race ###
+### FEMALE PAY BY RACE ###
 ##########################
 
 data = load_data("edu_wages_2022.csv")
