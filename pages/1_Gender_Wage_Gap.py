886f06749153686f34670d7e11d161d34762244f
mcverhulst <verhulst@umich.edu>
2023-04-15 18:27:24 -0400

    - standardized color scale - added area chart to hours needed tab

--- pages/1_Gender_Wage_Gap.py
+++ pages/1_Gender_Wage_Gap.py
@@ -95,11 +95,16 @@ with tab2:
     # DEFINE CHECKBOX OPTIONS
     cuts = ["total men", "total women", "White + Female", "Hispanic or Latino + Female",
             "Black + Female", "Asian + Female"]
+    # DEFINING COLOR SCALE
+    alt_colors = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"]
+    colors = alt.Scale(domain=(cuts), range=alt_colors)
+
     new = data_only[data_only['group'].isin(cuts)]
 
     race_checkboxes = new.group.unique()
 
     # COLLECT CHECKBOX VALUES
+    # https://stackoverflow.com/questions/74243710/how-can-we-create-this-type-of-filter-checkbox-in-streamlit-python
     # https://discuss.streamlit.io/t/horizontal-checkbox/32751
     val = [None]* len(race_checkboxes)
 
@@ -120,11 +125,14 @@ with tab2:
     # FILTER DATAFRAME TO ONLY CHECKED BOXES
     new_filt = new[new.group.isin(race_checkboxes[val])].reset_index(drop=True)
 
+
+
     title = alt.TitleParams("Median Weekly Earnings in 2022 Dollars", anchor='middle')
     races = alt.Chart(new_filt, title=title).mark_line(point=True).encode(
         x=alt.X('year:N', title='Year',axis=alt.Axis(labelAngle=-45)),
         y=alt.Y('value:Q', title="Median Weekly Wages", scale=alt.Scale(domain=(500, 1400))),
-        color='group'
+        # color='group'
+        color=alt.Color("group:N", scale=colors)
     )
 
     # PLOT CHART
@@ -136,14 +144,44 @@ with tab2:
 with tab3:
     title = alt.TitleParams("Hours Women Would Have to Work to Match Mens' Pay", anchor='middle')
 
-    hours = alt.Chart(ratio_df, title=title).mark_bar().encode(
+    hours = alt.Chart(ratio_df, title=title,height=500).mark_bar().encode(
         x = alt.X('year:O', title='Year', axis=alt.Axis(labelAngle=-45)),
         y = alt.Y('hours:Q', title='Hours'),
         color = alt.value("#de8b33")
     )
+
+    dummy_years = ["1995", "1996", "1997", "1998", "1999", "2000",
+                     "2001", "2002", "2003", "2004", "2005", "2006", "2007",
+                     "2008", "2009", "2010", "2011", "2012", "2013", "2014",
+                     "2015", "2016", "2017", "2018", "2019", "2020"]
+
+    dummy_weeks = {'weeks': 52, 'year': dummy_years}
+    d_weeks = pd.DataFrame(data=dummy_weeks)
+
+    dummy_months = {'months': 12, 'year': dummy_years}
+    d_months = pd.DataFrame(data=dummy_months)
+
+    ### MONTHS AREA CHART
+    m_months = alt.Chart(d_months, height=500).mark_area().encode(
+        alt.X("year:O", title="", axis=alt.Axis(labelAngle=-45)),
+        alt.Y('months:Q', title="", scale=alt.Scale(domain=(0, 12))),
+        color=alt.value("#1f77b4"),
+        tooltip=[]
+    )
+
+    w_months = alt.Chart(ratio_df,height=500).mark_area().encode(
+        alt.X("year:O", axis=alt.Axis(labelAngle=-45), title='Year'),
+        alt.Y("f_months:Q", title='Additional Months', scale=alt.Scale(domain=(0, 12))),
+        color=alt.value("#ff7f0e")
+    )
+
+    mw_months = m_months + w_months
 
     st.markdown("""### How many hours would women have to work to earn equal pay""")
     st.write("""Lorem ipsum...""")
 
     # PLOT CHART
     st.altair_chart(hours, theme="streamlit", use_container_width=True)
+
+    st.markdown("""### How many ***months*** into the next year would women have to work to earn equal pay?""")
+    st.altair_chart(mw_months, theme="streamlit", use_container_width=True)
